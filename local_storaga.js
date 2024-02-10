a108bec675aa7ca8266a682b3f77926b50dde042
shantulidhale <145424877+shantulidhale@users.noreply.github.com>
2024-02-10 13:15:10 +0530

    Add files via upload

---
+++ local_storaga.js
@@ -0,0 +1,114 @@
+// console.log("I am best programmer");
+
+// localStorage.setItem("firstName","shantuli.");
+// let name=localStorage.getItem("firstName");
+// console.log(name);
+// let impArray=['shanutli','kanchan','pratiksha','kavita'];
+// localStorage.setItem('bestname',JSON.stringify(impArray));
+// localStorage.setItem("second name","Dhale");
+// localStorage.setItem("company_name","ArrayPointer!");
+// let name1=localStorage.getItem("second name");
+// console.log(name1);
+// localStorage.setItem("thirdnmae","")
+// "use strict";
+
+// function example() {
+//     var variable1 = 42;
+//     variable2 = 33; // Throws a ReferenceError in strict mode because variable2 is undeclared.
+// }
+
+// example();
+
+
+
+// let age = prompt('how old are you ?', 500);
+// alert(`you are ${age} years old !`);
+
+// alert("10" * "2");
+// alert(Number("120"));
+// alert(Number("120s"));
+// alert(Number(true));
+// alert(Number(false));
+
+// alert(Boolean(1));
+// alert(Boolean(0));
+// alert(Boolean(" Hello Shantuli"));
+// alert(Boolean(""));
+
+// function shantuli() {
+//     let message = "Hello shantuli Dhale";
+//     alert(message);
+// }
+// shantuli();
+
+// let Name = "shantuli";
+// function oldMessage() {
+//     let gold = "hello," + Name;
+//     alert(gold);
+// }
+// oldMessage();
+
+// let myname = 'Diya';
+// function oldMessage() {
+//     myname = "Manavi";
+//     let message = "hii:" + myname;
+//     alert(message);
+// }
+// alert(myname);
+// oldMessage();
+// alert(myname);
+
+// let userName = "diya";
+// function showmessage() {
+//     let userName = "manavi";
+//     let message = 'hii,' + userName;
+//     alert(message);
+// }
+// showmessage();
+// alert(userName);
+
+// function showmessaage(from, text) {
+//     alert(from + ':' + text);
+// }
+// showmessaage('ANU', 'Hello');
+// showmessaage('and ', "what's up?")
+
+// let sum = (a, b) => a + b;
+// alert(sum(10, 20));
+
+// let double = n => n * 2;
+// alert(double(3));
+
+
+// let age = prompt("what is your age?");
+// let welcome = (age < 18) ?
+//     () => alert("hii") :
+//     () => alert("greeting");
+// welcome();
+
+// let sum = (a, b) => {
+//     let result = a * b;
+//     return result;
+
+// };
+// alert(sum(1, 2));
+
+
+// let shantuli = (a, b) => {
+//     let odd = a / b;
+//     return odd;
+// };
+// alert(shantuli(10, 2));
+
+// let multi = () => console.log("shantuli");
+// multi();
+
+// let welcome = (sirname, age) => `shantuli & manavi ${sirname} -${age}`;
+
+// console.log(welcome("dhale", 22));
+// console.log(typeof welcome);
+// open console to see
+// for (let i = 0; i < 5; i++) {
+//     debugger;
+//     console.log("value,", i);
+// }
